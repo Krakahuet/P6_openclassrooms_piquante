649fcaabfffa9615b870f695ee396f1e597c5560
Simon Polit <spolit@fluksaqua.com>
2022-12-13 20:23:05 +0100

    piquante

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use((req, res, next) => {
   });
 
 app.use('/api/sauces', saucesRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/auth', userRoutes);
 
 app.use(express.json());
 app.use(bodyParser.json());
